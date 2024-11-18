f4710e5fc88f2bce3e48378fe8c5cb36cfbaf9f5
danny9001 <danny9001@gmail.com>
2024-11-18 15:35:58 -0400

    Update database.php

--- database/database.php
+++ database/database.php
@@ -1,19 +1,29 @@
 <?php
-require_once 'config.php';
-
 class Database {
+    private $host;
+    private $username;
+    private $password;
+    private $database;
     private $conn;
 
     public function __construct() {
-        $this->conn = $conn;
+        $this->host = 'tu_host';
+        $this->username = 'eventos_user';
+        $this->password = '+J3tD4taBaS3R3serv4**';
+        $this->database = 'eventos';
+
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+        if ($this->conn->connect_error) {
+            die("Conexión fallida: " . $this->conn->connect_error);
+        }
+    }
+
+    public function prepare($sql) {
+        return $this->conn->prepare($sql);
     }
 
     public function query($sql) {
         return $this->conn->query($sql);
     }
-
-    public function close() {
-        $this->conn->close();
-    }
 }
-?>
