8afe6eca1e02d23be2269e0acb521e6ae8b4253f
danny9001 <danny9001@gmail.com>
2024-11-18 16:12:02 -0400

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,4 +1,9 @@
 <?php
 spl_autoload_register(function ($class) {
-    require_once $class . '.php';
+    require_once __DIR__ . '/' . $class . '.php';
 });
+
+// o
+
+require_once __DIR__ . '/database/Database.php';
+require_once __DIR__ . '/controllers/UsuarioController.php';
